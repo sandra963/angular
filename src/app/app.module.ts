51e66cfa0b99373f5770a6f16da0b30deabfbc1e
Sandra Vilca <sandra25vilca@gmail.com>
2024-05-12 23:00:47 -0500

    fin-seccion 3diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './share/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './share/sidebar/sidebar.component';
+import { HeaderComponent } from './share/header/header.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
